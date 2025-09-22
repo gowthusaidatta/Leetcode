3a1ea1c281712d4809ef532aa92bab9eceb8da2e
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-09-22 21:14:46 +0530

    Time: 2 ms (93.4%), Space: 56 MB (40.78%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five =0,ten = 0;
+
+        for(int bill : bills){
+            if(bill == 20){
+                if(ten > 0 && five > 0){
+                    ten --;
+                    five --;
+                }
+                else if(five >= 3){
+                    five -= 3;
+
+                }else return false;
+            }else if(bill == 10){
+                if(five >= 1){
+                    five--;
+                    ten++;
+                }else return false;
+            }else five++;
+        }
+
+        return true;
+        
+    }
+}
