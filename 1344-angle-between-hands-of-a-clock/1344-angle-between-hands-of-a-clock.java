34b86db7f5f0250272ebfc88202bf50dd1a3a7e8
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2026-06-21 17:22:31 +0530

    Time: 0 ms (100%), Space: 45 MB (48.58%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double m=6*minutes;
+        double h=30*(hour%12)+0.5*minutes;
+        double angle=Math.abs(h-m);
+        return Math.min(angle, 360 - angle);
+    }
+}
