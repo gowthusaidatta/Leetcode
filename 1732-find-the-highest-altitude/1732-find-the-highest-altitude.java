552bb27257584a540217cf5f03ff7d9d2acf4016
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2026-06-21 17:40:53 +0530

    Time: 12 ms (1.45%), Space: 43.6 MB (7.63%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,13 +1,13 @@
 class Solution {
     public int largestAltitude(int[] gain) {
         int sum=0;
-        int ans=0,max=Integer.MIN_VALUE;
+        int ans=0,max=0;
         for(int i=0;i<gain.length;i++){
             
             sum+=gain[i];
             max=Math.max(sum,max);
             System.out.println(sum);
         }
-        return max>-1?max:ans;
+        return max;
     }
 }
