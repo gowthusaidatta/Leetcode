343d47affa05c7fe8192a44d45f7bb1b05ab595d
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2026-06-22 10:32:15 +0530

    Time: 3 ms (67.53%), Space: 46.4 MB (70.27%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null)return false;
+        if(check(root,subRoot)){
+            return true;
+        }
+    
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    public boolean check(TreeNode root,TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        if(root==null || subRoot==null) return false;
+        return root.val==subRoot.val && check(root.left,subRoot.left) && check(root.right,subRoot.right);
+    }
+}
