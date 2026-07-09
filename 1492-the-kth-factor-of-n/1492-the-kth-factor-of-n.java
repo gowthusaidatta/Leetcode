c171c139aba1641a51197f7157d0dad2d3bf6faf
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2026-07-09 16:17:52 +0530

    Time: 1 ms (41.65%), Space: 42.4 MB (33.64%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,15 +1,14 @@
 class Solution {
     public int kthFactor(int n, int k) {
         List<Integer> list=new ArrayList<>();
-        for(int i=1;i*i<=n;i++){
+        int c=0;
+        for(int i=1;i<=n;i++){
             if(n%i==0){
-                list.add(i);
-                if(i!=n/i){
-                    list.add(n/i);
-                }
+               c++;
             }
+            if(k==c)return i;
         }
-        Collections.sort(list);
-        return k<=list.size()?list.get(k-1):-1;
+        
+        return -1;
     }
 }
