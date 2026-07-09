fdd232acb72516feae4969b5520809d00d0a94c1
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2026-07-09 16:43:43 +0530

    Time: 6 ms (88.14%), Space: 45.3 MB (63.79%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack=new Stack();
+        for(String s:tokens){
+            if(s.equals("+")){
+                int a=stack.pop();
+                int b=stack.pop();
+                stack.push(a+b);
+            }
+            else if(s.equals("-")){
+               int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            }
+            else if(s.equals("*")){
+                int a=stack.pop();
+                int b=stack.pop();
+                stack.push(a*b);
+            }
+            else if(s.equals("/")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            }
+            else{
+                stack.push(Integer.valueOf(s));
+            }
+
+        }
+        return stack.peek();
+    }
+}
