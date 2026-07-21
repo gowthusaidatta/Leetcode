6c64b208837d3bdf8067030b37d3ba524ebd339d
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2026-07-21 10:20:25 +0530

    Time: 12 ms (36.91%), Space: 52.4 MB (90.81%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        for(int n:map.keySet()){
+            if(map.get(n)>(nums.length/2)){
+                return n;
+            }
+        }
+        return 0;
+    }
+}
